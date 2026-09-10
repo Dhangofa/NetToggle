bd746ca874906b69ab8e8ab364f245d8377bf194
DK <117024745+Dhangofa@users.noreply.github.com>
2026-09-11 00:14:46 +0530

    Standardize Shizuku binder mode reader formatting
    - Normalized line endings and maintained Shizuku IPC binder queries for active network modes.

--- app/src/main/java/com/dhangofa/networktoggle/telephony/ShizukuBinderModeReader.java
+++ app/src/main/java/com/dhangofa/networktoggle/telephony/ShizukuBinderModeReader.java
@@ -32,7 +32,7 @@ final class ShizukuBinderModeReader {
         }
 
         int subId = simResolver.resolveTargetSubId(executionMode);
-        
+
         if (!simResolver.isValidSubId(subId)) {
             return NetworkMode.UNKNOWN;
         }
@@ -52,75 +52,75 @@ final class ShizukuBinderModeReader {
                 return NetworkMode.UNKNOWN;
             }
 
-            @SuppressLint("PrivateApi") 
+            @SuppressLint("PrivateApi")
             Class<?> stub = Class.forName("com.android.internal.telephony.ITelephony$Stub");
             Object phone = stub.getDeclaredMethod("asInterface", IBinder.class).invoke(null, binder);
-            
+
             Class<?> api = Class.forName("com.android.internal.telephony.ITelephony");
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 Method method = TelephonyMethodHelper.find(
-                        api, 
+                        api,
                         "getAllowedNetworkTypesForReason",
-                        new Class<?>[] {int.class, int.class}, 
-                        new Class<?>[] {int.class, int.class, String.class}, 
+                        new Class<?>[] {int.class, int.class},
+                        new Class<?>[] {int.class, int.class, String.class},
                         new Class<?>[] {int.class}
                 );
-                
+
                 if (method == null) {
                     return NetworkMode.UNKNOWN;
                 }
 
-                Object value = method.getParameterCount() == 2 
+                Object value = method.getParameterCount() == 2
                         ? method.invoke(phone, subId, 0)
-                        : method.getParameterCount() == 3 
-                                ? method.invoke(phone, subId, 0, PACKAGE) 
+                        : method.getParameterCount() == 3
+                                ? method.invoke(phone, subId, 0, PACKAGE)
                                 : method.invoke(phone, subId);
-                                
+
                 if (!(value instanceof Number)) {
                     return NetworkMode.UNKNOWN;
                 }
-                
+
                 long bitmask = ((Number) value).longValue();
-                
+
                 for (NetworkMode mode : NetworkMode.values()) {
                     if (mode.getBinaryMask() != null && Long.parseLong(mode.getBinaryMask(), 2) == bitmask) {
                         return mode;
                     }
                 }
-                
+
                 if ((bitmask & (1L << 19)) != 0) {
                     return (bitmask & (1L << 12)) == 0 ? NetworkMode.FIVE_G_ONLY : NetworkMode.PREFERRED_5G;
                 }
-                
+
                 if ((bitmask & (1L << 12)) != 0) {
-                    return (bitmask & (1L << 13)) == 0 && (bitmask & (1L << 9)) == 0 
-                            ? NetworkMode.FOUR_G_ONLY 
+                    return (bitmask & (1L << 13)) == 0 && (bitmask & (1L << 9)) == 0
+                            ? NetworkMode.FOUR_G_ONLY
                             : NetworkMode.PREFERRED_4G;
                 }
-                
+
                 return NetworkMode.PREFERRED_3G;
             }
 
             Method method = TelephonyMethodHelper.find(
-                    api, 
+                    api,
                     "getPreferredNetworkType",
-                    new Class<?>[] {int.class}, 
+                    new Class<?>[] {int.class},
                     new Class<?>[] {int.class, String.class}
             );
-            
+
             if (method == null) {
                 return NetworkMode.UNKNOWN;
             }
-            
-            Object value = method.getParameterCount() == 1 
-                    ? method.invoke(phone, subId) 
+
+            Object value = method.getParameterCount() == 1
+                    ? method.invoke(phone, subId)
                     : method.invoke(phone, subId, PACKAGE);
-                    
-            return value instanceof Number 
-                    ? NetworkMode.fromLegacyMode(((Number) value).intValue()) 
+
+            return value instanceof Number
+                    ? NetworkMode.fromLegacyMode(((Number) value).intValue())
                     : NetworkMode.UNKNOWN;
-                    
+
         } catch (Throwable ignored) {
             return NetworkMode.UNKNOWN;
         }
